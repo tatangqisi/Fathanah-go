6a5702f69930bcaea86ea89930148abef6da2e41
izaza97 <zauhairizaz@gmail.com>
2023-02-01 13:21:08 +0700

    fisrt commit

--- controllers/qurancontroller/qurancontroller.go
+++ controllers/qurancontroller/qurancontroller.go
@@ -49,12 +49,22 @@ func Shows(w http.ResponseWriter, r *http.Request) {
 	var dh []models.Qrn
 	Lg := []models.Head{}
 	Ic := []models.Icon{}
+	Srh := []models.Surah{}
+	Pck := []models.Picked{}
 	var response models.Dqrn
 	// dhInput := models.Dh{menu: int(menu)}
-	if err := models.DB.Table("quran-data").Where("`surah`= ?", surah).Find(&dh).Error; err != nil {
+	Pickedsurah := models.DB.Table("quran-surah").Where("id = ?", surah).Scan(&Pck).Error
+	if Pickedsurah != nil {
+		log.Print(Pickedsurah.Error())
+	}
+	if err := models.DB.Table("quran-data").Select("`quran-data`.`id`, `quran-surah`.`name`, `quran-data`.`arab`, `quran-data`.`latin`, `quran-data`.`meaning`").Joins("INNER JOIN `quran-surah` ON `quran-data`.`surah` = `quran-surah`.`id`").Where("`quran-data`.`surah` = ?", surah).Find(&dh).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		helper.ResponseJSON(w, http.StatusInternalServerError, response)
 		return
+	}
+	surat := models.DB.Table("quran-surah").Scan(&Srh).Error
+	if surat != nil {
+		log.Print(surat.Error())
 	}
 	logo := models.DB.Table("img-asset").Select("`img-asset`.`id`, `img-asset`.`name`, `img-asset`.`img`, `img-path`.`path`").Joins("INNER JOIN `img-path` ON `img-asset`.`path` =`img-path`.`id`").Where("`img-asset`.`name` = 'Fathanah'").Find(&Lg).Error
 	if logo != nil {
@@ -67,6 +77,8 @@ func Shows(w http.ResponseWriter, r *http.Request) {
 	}
 	response.Icon = Ic
 	response.Logo = Lg
+	response.Pickedsurah = Pck
+	response.Surah = Srh
 	response.Data = dh
 	w.Header().Set("Content-Type", "appication/json")
 	json.NewEncoder(w).Encode(response)
--- models/quran.go
+++ models/quran.go
@@ -2,16 +2,28 @@ package models
 
 type Qrn struct {
 	Id      int    `json:"id"`
-	Surah   string `json:"surah"`
+	Name    string `json:"surah"`
 	Arab    string `json:"arab"`
 	Latin   string `json:"latin"`
 	Meaning string `json:"arti"`
 }
 
+type Picked struct {
+	Id   int    `json:"id"`
+	Name string `json:"surah"`
+}
+
+type Surah struct {
+	Id   int    `json:"id"`
+	Name string `json:"surah"`
+}
+
 type Dqrn struct {
-	Icon []Icon
-	Logo []Head
-	Data []Qrn
+	Icon        []Icon
+	Logo        []Head
+	Pickedsurah []Picked
+	Surah       []Surah
+	Data        []Qrn
 }
 
 type Qrns struct {
